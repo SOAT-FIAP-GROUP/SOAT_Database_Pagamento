ef1fac7f764142d4613c5e0b939b89741a4a5ecd
JoseAugustoDosSantos <augusto03jose@gmail.com>
2026-01-05 18:54:21 -0300

    ajuste de integracao com o eks

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = local.region
+  region = "us-east-1"
 }
 
 terraform {
   backend "s3" {
-    bucket         = "meu-terraform-states-soat"
-    key            = "env:/dev/rds/terraform.tfstate"
+    bucket         = "meu-terraform-states-soat-948512815388"
+    key            = "env:/dev/dynamo/pagamento/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks-soat"
   }
