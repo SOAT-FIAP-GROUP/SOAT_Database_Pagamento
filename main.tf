926ae2704bffb6afdcbccbd23d4df90b2172db48
JoseAugustoDosSantos <augusto03jose@gmail.com>
2026-01-08 16:23:28 -0300

    adicao database entrega e fila

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+#==================================================
+#--------------- Dynamo Pagamento -----------------
+#==================================================
+
 resource "aws_dynamodb_table" "pagamento" {
   name           = "Pagamento"
   billing_mode   = "PAY_PER_REQUEST"
@@ -14,6 +18,65 @@ attribute {
 
   tags = {
     Name        = "dynamodb-table-pagamento"
+    Environment = "dev"
+  }
+}
+
+#==================================================
+#---------------- Dynamo Entrega ------------------
+#==================================================
+
+resource "aws_dynamodb_table" "producao_entrega" {
+  name           = "Entrega"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "codigo"
+
+attribute {
+  name = "codigo"
+  type = "S"
+}
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+
+  tags = {
+    Name        = "dynamodb-table-producao-entrega"
+    Environment = "dev"
+  }
+}
+
+#==================================================
+#------------- Dynamo Fila de Pedido --------------
+#==================================================
+
+resource "aws_dynamodb_table" "producao_fila_pedido" {
+  name           = "Fila_Pedido"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "codigo"
+
+attribute {
+  name = "codigo"
+  type = "S"
+}
+
+attribute {
+  name = "pedidoCodigo"
+  type = "S"
+}
+
+global_secondary_index {
+  name            = "pedidoCodigo"
+  hash_key        = "pedidoCodigo"
+  projection_type = "ALL"
+}
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+
+  tags = {
+    Name        = "dynamodb-table-producao-fila-pedido"
     Environment = "dev"
   }
 }
